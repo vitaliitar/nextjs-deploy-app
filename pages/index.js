eb9982aa04f16ca43ab9c4223d95244879229323
Vitalii Tar <vitally.tar25@gmail.com>
2022-11-05 21:59:42 +0200

    chore: add deploy to vercel test

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      Hello NextJS deploy app
+      Hello NextJS deploy app updated
     </div>
   )
 }
